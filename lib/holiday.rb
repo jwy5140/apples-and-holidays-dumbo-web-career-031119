a474a87fe88235010f3fe920f526854a61dda40e
Jeffrey Yu <jwy5140@gmail.com>
2019-02-24 07:36:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.keys("BBQ")
+  holiday_hash.each {|x,y| 
+    y.each {|x,y|
+      x.keys(y.include?("BBQ"))
+    }
+  }
 end
 
 
