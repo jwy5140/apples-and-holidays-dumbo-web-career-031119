446ff6bf8fb311b612f6c1f33c880d0432db2eda
Jeffrey Yu <jwy5140@gmail.com>
2019-02-24 07:09:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
       x = x.to_s
       if (x.include?("_"))
         a = ((x.split(/\W/)).each {|x| x.capitalize!}).join(" ")
+        binding.pry
       else
         a = "  #{x.capitalize!}: "
       end
